26f9d31a1163585687f4f44a75560815d46bb93b
shiziwen <shiziwen@gmail.com>
2016-03-02 23:56:27 +0800

    fix bugs

--- 7-reverse_integer/solution.py
+++ 7-reverse_integer/solution.py
@@ -4,18 +4,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+
         a = 0
         b = x if x > 0 else -x
         while b > 0:
             a = a * 10 + b % 10
             b = b / 10
+        
         if x < 0:
-            if x < - (2 ** 31):
+            if a > (2 ** 31):
                 return 0
             else:
                 return -a
         else:
-            if x > (2 ** 31 - 1):
+            if a > (2 ** 31 - 1):
                 return 0
             else:
                 return a
