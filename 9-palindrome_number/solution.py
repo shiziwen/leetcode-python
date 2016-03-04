a85be22f2320351b4eb1405df0855c32e58cc06e
shiziwen <shiziwen@gmail.com>
2016-03-05 00:28:29 +0800

    modified 9-palindrome_number

--- 9-palindrome_number/solution.py
+++ 9-palindrome_number/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     # @return a boolean
     def isPalindrome(self, x):
-        if x <= 0:
-            return False if x < 0 else True
+        if len(str(x)) == 1:
+            return True
+        if x < 0:
+            return False
         a, b = x, 0
         while a:
             b, a = b * 10 + a % 10, a / 10
