5f3106c446354833611be39335320445ee97ccc4
shiziwen <shiziwen@gmail.com>
2016-03-12 23:58:36 +0800

    added test

--- 17-letter_commbinations_of_a_phone_number/solution.py
+++ 17-letter_commbinations_of_a_phone_number/solution.py
@@ -16,9 +16,16 @@ class Solution(object):
     def combination(self, digits, index, tmp):
         if index == len(digits):
             self.res.append("".join(tmp))
+            print 'res is %s' % self.res
             return
         for c in self.dlist[ord(digits[index]) - ord('0')]:
             tmp.append(c)
+            print '1-tmp is %s' % tmp
             self.combination(digits, index + 1, tmp)
             tmp.pop()
+            print '2-tmp is %s' % tmp
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.letterCombinations("23")
 
