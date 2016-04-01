d27c33864776a3cb7ca44fc39bc7961712c7019c
shiziwen <shiziwen@gmail.com>
2016-04-01 23:55:35 +0800

    added 34-search_for_a_range

---
+++ 34-search_for_a_range/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) / 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                list = [-1, -1]
+                if A[left] == target: list[0] = left
+                if A[right] == target: list[1] = right
+                for i in range(mid, right+1):
+                    if A[i] != target: list[1] = i - 1; break
+                for i in range(mid, left-1, -1):
+                    if A[i] != target: list[0] = i + 1; break
+                return list
+        return [-1, -1]
