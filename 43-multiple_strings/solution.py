9a1f6f89a27bbf4b60b18e783ce2455e63a291b5
shiziwen <shiziwen@gmail.com>
2016-04-09 23:56:07 +0800

    added 43-multiple_strings

---
+++ 43-multiple_strings/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        arr = [0 for i in range(len(num1) + len(num2))]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                arr[i+j] += int(num1[i]) * int(num2[j])
+        res = []
+        for i in range(len(arr)):
+            digit = arr[i] % 10
+            carry = arr[i] / 10
+            if i < len(arr) - 1:
+                arr[i+1] += carry
+            res.insert(0, str(digit))
+        if res[0] == '0' and len(res) > 0:
+            del res[0]
+        return ''.join(res)
