717bcfb94fb94ff87338cf86992029263b59eb88
shiziwen <shiziwen@gmail.com>
2016-04-15 23:20:50 +0800

    added 49-group_anagrams

---
+++ 49-group_anagrams/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict = {}
+        for word in strs:
+            sortedword = ''.join(sorted(word))
+            dict[sortedword] = [word] if sortedword not in dict else dict[sortedword] + [word]
+        res = []
+        for item in dict:
+            # if len(dict[item]) >= 2:
+            res.append(dict[item])
+        return res
