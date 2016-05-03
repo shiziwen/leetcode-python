bd185792d79a6cf9c5f131c0f92bb9db15e24d89
shiziwen <shiziwen@gmail.com>
2016-05-03 23:27:28 +0800

    added 62-unique_paths

---
+++ 62-unique_paths/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 and n == 1:
+            list = [[1]]
+        elif m == 1 and n > 1:
+            list = [[1 for i in range(n)]]
+        elif m > 1 and n == 1:
+            list = [[1] for i in range(m)]
+        else:
+            list = [[0 for i in range(n)] for i in range(m)]
+            for i in range(0, n):
+                list[0][i] = 1
+            for i in range(0, m):
+                list[i][0] = 1
+            for i in range(1, m):
+                for j in range(1, n):
+                    list[i][j] = list[i-1][j] + list[i][j-1]
+        return list[m-1][n-1]
