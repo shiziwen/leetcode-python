9727a9f10ab96332a5d7b5bb67343d17f979c523
shiziwen <shiziwen@gmail.com>
2016-05-14 23:52:59 +0800

    added 73-set_matrix_zeroes

---
+++ 73-set_matrix_zeroes/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        rownum = len(matrix)
+        colnum = len(matrix[0])
+        row = [False for i in range(rownum)]
+        col = [False for i in range(colnum)]
+        for i in range(rownum):
+            for j in range(colnum):
+                if matrix[i][j] == 0:
+                    row[i] = True
+                    col[j] = True
+        for i in range(rownum):
+            for j in range(colnum):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
